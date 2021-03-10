ed191ffd60861f8b47ea2c85b9515ed75bfcb5da
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-03-10 16:07:00 +0000

    Fix(binary_tree_is_root): change if statement to evaluate 1 node tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,7 +11,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 
 	if (node)
 	{
-		if (!node->parent && (node->left || node->right))
+		if (!node->parent)
 			is_root = 1;
 	}
 	return (is_root);
