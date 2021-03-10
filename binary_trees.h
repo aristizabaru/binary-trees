865558860b317aa1539fb0219d4cd9a9363ab455
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-03-10 23:31:28 +0000

    Add(binary_tree_size): add prototype

--- binary_trees.h
+++ binary_trees.h
@@ -39,5 +39,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t height_b(const binary_tree_t *tree, size_t level);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
