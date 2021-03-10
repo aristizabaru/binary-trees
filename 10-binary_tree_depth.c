3f99c6526b5d68cedb9411d0093f0275492a52fd
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-03-10 19:58:17 +0000

    Feat: add check to return depth without addition when tree is 1 node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,10 +10,12 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	/* depth of tree */
 	depth = binary_tree_depth(tree->parent);
 
-	return (depth + 1);
+	if (tree->parent)
+		return (depth + 1);
+	return (depth);
 }
